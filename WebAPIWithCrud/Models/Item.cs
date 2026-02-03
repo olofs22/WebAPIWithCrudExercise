29b5d902563ba18152e05b510d9de64075053940
olofs22 <olof.business.svensson@gmail.com>
2026-02-03 17:36:06 +0100

    DataAnnotations Validation testing

--- WebAPIWithCrud/Models/Item.cs
+++ WebAPIWithCrud/Models/Item.cs
@@ -1,9 +1,12 @@
-namespace WebAPIWithCrud.Models;
+using System.ComponentModel.DataAnnotations;
 
+namespace WebAPIWithCrud.Models;
 public class Items
 {
+    [Required, Range(1, 20)]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = "";
-
+    [StringLength (15)]
     public string Description { get; set; } = "";
 }
